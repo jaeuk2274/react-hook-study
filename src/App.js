19337e1e3b6f43024f549fbb1a6d1be23d8fd43f
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-28 17:08:22 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+export default function App() {
+  const [number, setNumber] = useState(0);
+  const addNumber = () => setNumber(number + 1);
+  const minusNumber = () => setNumber(number - 1);
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Number : {number} </h2>
+      <button onClick={addNumber}> add </button>
+      <button onClick={minusNumber}> minus </button>
+      <input {...name} />
     </div>
   );
 }
-
-export default App;
