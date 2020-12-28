a3c56367ce3c8e40b5b228f25e49e475978f2242
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-28 20:27:30 +0900

    Depart useInput

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
+import { useInput } from "./useInput";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
-  };
-  return { value, onChange };
-};
 
 export default function App() {
   const [number, setNumber] = useState(0);
