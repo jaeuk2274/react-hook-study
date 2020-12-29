b96a2893bf59d5e375d3b71e151a96d1f6df65e2
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-29 10:16:34 +0900

    create useClick

--- src/App2.js
+++ src/App2.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./styles.css";
 import { useTitle } from "./useTitle";
+import { useClick } from "./useClick";
+
+
 
 export const App2 = () => {
   //useTitle
   const titleUpdater = useTitle("Loading...");
   setTimeout(() => titleUpdater("Home"), 5000); // 5초 뒤 타이틀 변경
 
+  //useClick
+  const sayHello = () => console.log("say~~~~~");
+  const title = useClick(sayHello);
+
   return (
     <div className="App2">
-        <div>hi</div>
+        <h1 ref={title}>Hi</h1>
     </div>
   );
 }
+
---
+++ src/useClick.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef } from "react";
+
+export const useClick = onClick => {
+    const element = useRef();
+  
+    useEffect(() => {
+      // componentDidMount() 1회성 
+      if(element.current){ // 값 update 시에는 미실행, depence
+        element.current.addEventListener("click", onClick);
+      }
+      return () => {
+        if(element.current){
+          // componentWillUnmount(): 컴포넌트를 DOM에서 제거하기 전에 실행
+          // 이벤트 제거 및 정리 작업 수행 가능
+          element.current.removeEventListener("click", onClick);
+        }
+      }
+    }, []); // depence 없기 때문에 1회성
+  
+    if(typeof onClick !== "function") {
+      return;
+    }
+    
+    return element;
+  }
