daab1e258310a8d2dc4f2ea054f549fc25890dbc
jaeuk2274 <jaeuk2274@gmail.com>
2020-12-30 10:10:17 +0900

    create useNotification

--- src/App4.js
+++ src/App4.js
@@ -1,23 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
-import { useFullscreen } from "./useFullscreen";
+import { useNotification } from "./useNotification";
+
+
 
 
 export const App4 = () => {
- 
-  //useFullscreen 
-  const onFullS = isFull => {
-    console.log(isFull ? "We are full" : "We are small");
-  }
-  const {element, triggerFull, exitFull} = useFullscreen(onFullS);
-
+  const triggerNotIf = useNotification("Notification title", {body:"Notification description"});
   return (
-    <div className="App4" style = {{height:1500}}>
-        <div ref={element}>
-          <img src="https://item.kakaocdn.net/do/8bdb8dec2d96f5e334479d9e139a892cf43ad912ad8dd55b04db6a64cddaf76d" style ={{height:150}} /> 
-        </div>
-        <button onClick={triggerFull}> FULL BUTTON </button>
-        <button onClick={exitFull}> EXIT BUTTON </button>
+    <div className="App4">
+        <button onClick={triggerNotIf}> Notification </button>
+        <br />
+
     </div>
     
   );
---
+++ src/useNotification.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export const useNotification = (title, option) => {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+    const fireNotif = () => {
+      if(Notification.permission !== 'granted') {
+        Notification.requestPermission().then(permission => {
+          if(permission === "granted"){
+            new Notification(title, option);
+          } else{
+            return;
+          }
+        })
+      } else{
+        new Notification(title, option); 
+      }
+    }
+    return fireNotif;
+  }
